a447fd0662b2651e78ac12ea7a14c0494adbf5e1
UsmanHaiderWeb <usmanhaiderweb2005@gmail.com>
2024-11-25 10:11:53 +0500

    updating create blog controller to remove caption

--- controllers/createBlog.controller.js
+++ controllers/createBlog.controller.js
@@ -3,8 +3,8 @@ import uploadImageToCloudinary from '../helpers/cloudinary.helper.js'
 
 const createBlogController = async (req, res) => {
     try {
-        const { title, caption, description, categories, tags } = req.body;
-        if (!title || !caption || !description) {
+        const { title, description, categories, tags } = req.body;
+        if (!title || !description) {
             return res.status(400).json({ message: 'Some required fields are missing' });
         }
 
@@ -18,7 +18,6 @@ const createBlogController = async (req, res) => {
 
         const newBlog = await Blog.create({
             title,
-            caption,
             description,
             categories,
             tags,
@@ -39,12 +38,3 @@ const createBlogController = async (req, res) => {
 };
 
 export default createBlogController;
-/*
-Hirrd is a free job portal web application. It offers a lot of services that are mind blowing. We offer you complete convenience. User can register as a candidate, who is there is seek jobs, or as a recruiter, who is there to recruit such candidates that prove beneficial for their company.
-Hirrd is a free job portal web application. It offers a lot of services that are mind blowing. We offer you complete convenience. User can register as a candidate, who is there is seek jobs, or as a recruiter, who is there to recruit such candidates that prove beneficial for their company. If user is a candidate, he can save a job, apply for a job, and can view the growth of his applications that he submitted.
-But if he is a recruiter, he can create a job, and handle the applications (cancel or accept) that are submitted for the job hiring.
-
-jobs, job seeking, job hiring, internship, startup, tech jobs, web jobs, frontend jobs, backend jobs, developer jobs
-
-#jobs #jobseeking #jobhiring #internship #startup #techjobs #webjobs #frontendjobs #backendjobs #developerjobs
-*/
